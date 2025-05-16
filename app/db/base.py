932ec229791402418a602bc529f1b2137a64d876
YasmaniGarcia-blip <smile06yd@gmail.com>
2025-05-16 15:00:00 -0500

    Primer Commit

---
+++ app/db/base.py
@@ -0,0 +1,33 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está definida en el entorno")
+
+engine = create_async_engine(DATABASE_URL, echo=False, connect_args={"ssl": True}) # cambiar a True  para primera vez que se ejecute el proyecto el echo=True para ver las consultas SQL 
+SessionLocal = async_sessionmaker(
+    engine,  
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+Base = declarative_base()
+
+async def init_db():
+    async with engine.begin() as conn:
+        import app.db.models.empleado
+        import app.db.models.sucursal
+        import app.db.models.vehiculo
+        import app.db.models.vehiculo_sucursal
+        import app.db.models.rol
+        import app.db.models.usuario
+        await conn.run_sync(Base.metadata.create_all)
+async def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        await db.close()
